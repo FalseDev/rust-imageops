9087b8c45892797f8e77b2e4d97dcf5ebc1dcee1
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-05-30 16:42:28 +0530

    Added lib build

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,14 @@ use pyo3::types::PyBytes;
 use pyo3::wrap_pyfunction;
 use std::io::Cursor;
 
-#[pyfunction]
-fn multi_paste(base: Vec<u8>, paste_layers: Vec<(Vec<u8>, Vec<(u32, u32)>)>) -> PyResult<PyObject> {
+pub use image::ImageFormat as ImageFormat;
+
+type PasteLayer = (Vec<u8>, Vec<(u32, u32)>);
+
+pub fn multi_paste(
+    base: Vec<u8>,
+    paste_layers: Vec<PasteLayer>,
+) -> Result<image::DynamicImage, std::io::Error> {
     let mut base_image = Reader::new(Cursor::new(&base))
         .with_guessed_format()?
         .decode()
@@ -20,7 +26,20 @@ fn multi_paste(base: Vec<u8>, paste_layers: Vec<(Vec<u8>, Vec<(u32, u32)>)>) -> 
             image::imageops::overlay(&mut base_image, &layer_image, coordinates.0, coordinates.1);
         }
     }
+    Ok(base_image)
+}
 
+
+pub fn paste(
+    base: Vec<u8>,
+    layer: Vec<u8>,
+    coordinates: (u32, u32),
+) -> Result<image::DynamicImage, std::io::Error> {
+    multi_paste(base, vec![(layer, vec![coordinates])])
+}
+
+
+fn to_python_bytes(base_image: image::DynamicImage) -> PyResult<PyObject> {
     let mut output_bytes: Vec<u8> = Vec::new();
     base_image
         .write_to(&mut output_bytes, image::ImageFormat::Png)
@@ -34,14 +53,20 @@ fn multi_paste(base: Vec<u8>, paste_layers: Vec<(Vec<u8>, Vec<(u32, u32)>)>) -> 
     Ok(py_bytes.to_object(py))
 }
 
-#[pyfunction]
-fn paste(base: Vec<u8>, layer: Vec<u8>, coordinates: (u32, u32)) -> PyResult<PyObject> {
-    multi_paste(base, vec![(layer, vec![coordinates])])
-}
 
 #[pymodule]
 fn imageops(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(multi_paste, m)?)?;
-    m.add_function(wrap_pyfunction!(paste, m)?)?;
+    #[pyfn(m, "multi_paste")]
+    fn multi_paste_py(base: Vec<u8>, paste_layers: Vec<PasteLayer>) -> PyResult<PyObject> {
+        to_python_bytes(multi_paste(base, paste_layers)?)
+    }
+
+    #[pyfn(m, "paste")]
+    fn paste_py(base: Vec<u8>, overlay: Vec<u8>, coordinates: (u32, u32)) -> PyResult<PyObject> {
+        to_python_bytes(paste(base, overlay, coordinates)?)
+    }
+
+    m.add_function(wrap_pyfunction!(multi_paste_py, m)?)?;
+    m.add_function(wrap_pyfunction!(paste_py, m)?)?;
     Ok(())
 }
